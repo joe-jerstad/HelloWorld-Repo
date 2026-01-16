6b8c529d9cc593acac552cb9d987676bbb2ed881
joe-jerstad <vf4272vt@go.minnstate.edu>
2026-01-16 10:41:24 -0600

    Added Week 1 Problem Set

---
+++ basic_io.py
@@ -0,0 +1,65 @@
+'''
+year = int(input('Enter year: '))
+month = input('Enter month: ')
+day = int(input('Enter day: '))
+
+print(f'The date is {month} {day}, {year}.')
+'''
+
+'''
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+print(f'Hello {name}, {age} is a great age!')
+'''
+
+'''
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+print(f'Hello {first_name} {last_name}, see you later!')
+'''
+
+'''
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+print(f'Hello {first_name} {last_name}, how are you?')
+'''
+
+'''
+color = input('Enter a color: ')
+animal = input('Enter an animal: ')
+
+print(f'You picked a {color} {animal}')
+'''
+
+'''
+food = input('Enter a food: ')
+drink = input('Enter a drink: ')
+
+print(f'Your order is a {food} and a {drink}.')
+'''
+
+'''
+color = input('Enter a color: ')
+food = input('Enter a foodL: ')
+
+print(f'I also like {color} and {food}!')
+'''
+
+'''
+city = input('Enter a city: ')
+state = input('Enter a state: ')
+
+print(F'You are currently in {city}, {state}.')
+'''
+
+'''
+x = 3
+y = 7
+
+z = x
+x = y
+y = z
+
+print(f'y = {y}')
+print(f'x = {x}')
+'''
