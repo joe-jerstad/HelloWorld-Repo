ae1ed45a1dc0993a89c38f78121d1ddf9f747eb1
joe-jerstad <vf4272vt@go.minnstate.edu>
2026-01-17 15:25:41 -0600

    Used f string formatting to make cleaner multiplication table.

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,6 +1,7 @@
 #multiplication table
 #takes in rows and columns and prints out a multiplication table of the two
 
+'''
 def multiplication_table(rows, columns):
     for i in range(1, rows + 1):
         row = ''
@@ -12,12 +13,18 @@ def multiplication_table(rows, columns):
                 row += (f'{mul_num}  ')
             elif mul_num >= 100:
                 row += (f'{mul_num} ')
-
         print(f'{row}')
-        
+'''
+
+def multi_table(rows, columns):
+    for i in range(1, rows + 1):
+        for x in range(1, columns + 1):
+            print(f'{i * x:4}', end = '')
+        print()
+
 def main():
     row =  int(input('Enter the number for the rows: '))
     column =  int(input('Enter the number for the columns: '))
-    multiplication_table(row, column)
+    multi_table(row, column)
 
 main()
